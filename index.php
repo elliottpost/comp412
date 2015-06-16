5647992b62f9dbfb2170638ca7b40a5aae45d478
elliottpost <elliottpost@gmail.com>
2015-06-16 14:15:47 -0500

    Removed data archive file. Added csvs to web server and using file streaming to parse

--- index.php
+++ index.php
@@ -6,16 +6,22 @@ namespace elly;
 define( "CSV_PATH", "data" . DIRECTORY_SEPARATOR );
 
 //get the data
-$censusData = fgetcsv( CSV_PATH . "census-data.csv" );
-$foodData = fgetcsv( CSV_PATH . "food-inspections.csv" );
+$censusCsvRemote = "http://projects.ellytronic.media/homework/comp412/hw2/data/census-data.csv";
+$foodCsvRemote = "http://projects.ellytronic.media/homework/comp412/hw2/data/food-inspections.csv";
+$censusCsvLocal = CSV_PATH . "census-data.csv";
+$foodCsvLocal = CSV_PATH . "food-inspections.csv";
+$censusHandle = fopen( $censusCsvRemote, 'r' );
+$foodHandle = fopen( $foodCsvRemote, 'r' );
+$censusData = fgetcsv( $censusHandle );
+$foodData = fgetcsv( $foodHandle );
 
 //basic formatting if xdebug not installed
 if( !function_exists( 'xdebug_get_code_coverage' ) )
 	echo "<pre>";
 
 //dump the data
-var_dump( CSV_PATH . "food-inspections.csv", $censusData, $foodData );
+var_dump( $censusData, $foodData );
 
 //close formatting
 if( !function_exists( 'xdebug_get_code_coverage' ) )
-	echo "</pre>";
+	echo "</pre>"; 
