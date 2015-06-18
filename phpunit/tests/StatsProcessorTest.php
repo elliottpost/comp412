f9bfbebda1df49d95528a3b07bc0b28f3889f206
elliottpost <elliottpost@gmail.com>
2015-06-17 23:21:56 -0500

    Added std deviation testing

---
+++ phpunit/tests/StatsProcessorTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace elly;
+
+class FileParserTest extends \PHPUnit_Framework_TestCase {
+	
+	/**
+	 * @var Commnuity $obj: our Community
+	 */
+	protected $obj;
+
+	protected function setUp() {
+		new ProjectAutoload;
+		$this->obj = new StatsProcessor;
+	}
+
+	/**
+	 * ensures CSV files are read into assoc arrays correctly
+	 */
+	public function testCalculateStdDev() {
+		$numbers = array( 27,54,60,17,1,19,74,97,95,8 );
+		$this->assertEquals( 33, floor( $this->obj->calculateStdDev( $numbers, false ) ) );
+		$this->assertEquals( 35, floor( $this->obj->calculateStdDev( $numbers, true ) ) );
+	}
+
+
+	protected function tearDown() {
+
+	}
+}
